5c068205e0f08cc216459aaa4fd9fb05d9967bb9
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-02-24 01:35:32 -0400

    Cambios de Documentación y de Interfaz

--- src/edd/proyecto/Nodo.java
+++ src/edd/proyecto/Nodo.java
@@ -7,10 +7,26 @@ package edd.proyecto;
 /**
  *
  * @author pedro
+ * Clase Nodo
  */
+
 public class Nodo {
+    
+    /**
+     * Atributos de la Clase Nodo
+     * 
+     * element: 
+     * next: Apuntador al Nodo siguiente
+     */
+    
     private String element;
     private Arista next;
+    
+    
+    /**
+     * Constructor del Nodo
+     * @param element que contendrá el Nodo
+     */
     
     public Nodo(String element){
         this.element = element;
@@ -18,13 +34,15 @@ public class Nodo {
     }
 
     /**
-     * @return the element
+     * Getter del element del Nodo
+     * @return the element del Nodo
      */
     public String getElement() {
         return element;
     }
 
     /**
+     * Setter del element del Nodo
      * @param element the element to set
      */
     public void setElement(String element) {
@@ -32,6 +50,7 @@ public class Nodo {
     }
 
     /**
+     * Getter de la Arista siguiente
      * @return the next
      */
     public Arista getNext() {
@@ -39,7 +58,8 @@ public class Nodo {
     }
 
     /**
-     * @param next the next to set
+     * Setter de la Arista siguiente
+     * @param next Nueva Arista
      */
     public void setNext(Arista next) {
         this.next = next;
